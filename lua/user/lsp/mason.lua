df81c66931a5b25b9ddff4ff46cc26abd9f5fbf4
gagata <agataxgolebiowska@gmail.com>
2023-09-15 12:14:19 +0000

    minimal lualine

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -70,3 +70,19 @@ lspconfig.lua_ls.setup({
 		},
 	},
 })
+
+lspconfig.pyright.setup({
+	settings = {
+		pyright = {
+			autoImportCompletion = true,
+		},
+		python = {
+			analysis = {
+				autoSearchPaths = true,
+				diagnosticMode = "openFilesOnly",
+				useLibraryCodeForTypes = true,
+				typeCheckingMode = "off",
+			},
+		},
+	},
+})
